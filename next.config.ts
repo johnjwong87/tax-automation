1df659a8437f1848950f1d89ad6997cfbb7e4bc7
johnjwong87 <johnjwong87@gmail.com>
2026-01-20 23:33:10 -0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/apps/S216',
-  // assetPrefix: '/apps/S216', // Usually basePath handles this, but including for robustness
+  /* basePath: '/apps/S216', */
 };
 
 export default nextConfig;
